1d5dbb3aebbb74d7eef6ecb515147296a3bb289d
tanvir-taqi <taqit3852@gmail.com>
2025-02-22 00:46:20 +0600

    environment set up, file register and enqueue doen

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php 
+
+// functions
+
+// Theme Title
+add_theme_support("title-tag");
+
+
+//theme css and jquery file calling
+function css_js_file_calling(){
+
+    // styles
+wp_enqueue_style("info_style", get_stylesheet_uri());
+wp_register_style("bootstrap", get_template_directory_uri() . '/styles/bootstrap.css', array(), '5.3.3', 'all');
+wp_enqueue_style("bootstrap");
+wp_register_style("custom", get_template_directory_uri() . '/styles/custom.css', array(), '1.0.0', 'all');
+wp_enqueue_style("custom");
+
+    // scripts  
+    wp_enqueue_script("jquery");
+    wp_enqueue_script("bootstrap_script", get_template_directory_uri() . '/js/bootstrap.js', array(), '5.3.3', true);
+    wp_enqueue_script("main", get_template_directory_uri() . '/js/main.js', array(), '1.0.0', true);
+}
+add_action("wp_enqueue_scripts","css_js_file_calling");
